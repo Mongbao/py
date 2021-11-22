969a69971baf3c65f3d77b3f3784a912d82e9b12
Bao <4a13a003@stust.edu.tw>
2021-11-23 01:03:48 +0800

    ok

--- open.py
+++ open.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-from openpyxl import Workbook
-import time
-
-wb=Workbook()
-sheet=wb.active
-
-sheet['A1']=2020
-sheet['A2']='hello'
-sheet['A3']=41.89
-sheet['A4']=10
-
-now=time.strftime("%x")
-sheet['A5']=now
-
-wb.save("demo.xlsx")
-print('End')
-=======
 from openpyxl import Workbook
 import time
 
@@ -32,4 +14,3 @@ sheet['A5']=now
 
 wb.save("demo.xlsx")
 print("End")
->>>>>>> fe9e2dd1896dc4ab817a792b633e8329919aee2a
