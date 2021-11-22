7a2775339adafc8c696ebebfa083957c1d691ea1
Bao <4a13a003@stust.edu.tw>
2021-11-23 01:00:55 +0800

    ok

---
+++ open.py
@@ -0,0 +1,16 @@
+from openpyxl import Workbook
+import time
+
+wb=Workbook()
+sheet=wb.active
+
+sheet['A1']=2020
+sheet['A2']='hello'
+sheet['A3']=41.89
+sheet['A4']=10
+
+now=time.strftime("%x")
+sheet['A5']=now
+
+wb.save("demo.xlsx")
+print('End')
