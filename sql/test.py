a44fb08adca67f1e003877fce5284f7621ca76dd
Bao <4a13a003@stust.edu.tw>
2021-11-24 14:13:18 +0800

    pymssql test

--- sql/test.py
+++ sql/test.py
@@ -1,13 +1,14 @@
 import pymssql
 
-#server 資料庫伺服器名稱或IP    (192.168.0.30\SQLEXPRESS)
+#server 資料庫伺服器名稱或IP    (192.168.0.30\SQLEXPRESS)(114.35.131.81\SQLEXPRESS)
 #user   使用者名稱             (sa)
 #password   密碼               (sa)
 #database   資料庫名稱          (Northwind)
 #conn = pymssql.connect(server,user,userpassword,database)
 
 conn = pymssql.connect(
-    host='192.168.0.30\SQLEXPRESS',
+    #host='192.168.0.30\SQLEXPRESS',
+    host='114.35.131.81\SQLEXPRESS',
     user='sa',
     password='sa',
     database='Northwind')
