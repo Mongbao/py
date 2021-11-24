59cff836ea12d1fdffd977ed473a9975b7dab157
Mongbao <4a13a003@stust.edu.tw>
2021-11-24 23:49:03 +0800

    host sa passwd reset

--- sql/test.py
+++ sql/test.py
@@ -1,17 +1,17 @@
 import pymssql
-import re
+#import re
 
 #server 資料庫伺服器名稱或IP    (192.168.0.30\SQLEXPRESS)(114.35.131.81\SQLEXPRESS)
 #user   使用者名稱             (sa)
-#password   密碼               (sa)
+#password   密碼               (sa1234)
 #database   資料庫名稱          (Northwind)
 #conn = pymssql.connect(server,user,userpassword,database)
 
 conn = pymssql.connect(
-    host='192.168.0.30/\/SQLEXPRESS',
-
+    host='192.168.0.30\SQLEXPRESS',
+    #host='114.35.131.81:1433\SQLEXPRESS',
     user='sa',
-    password='sa',
+    password='sa1234',
     database='Northwind')
 #if connect:
 #    print("連線成功")
